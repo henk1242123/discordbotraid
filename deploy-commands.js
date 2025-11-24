144731bc39c82b2423d48fd858882fc7e935dbf6
henk1242123 <134098086+henk1242123@users.noreply.github.com>
2025-11-24 23:35:21 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,39 +1,47 @@
-import { REST, Routes, SlashCommandBuilder } from "discord.js";
-import "dotenv/config";
+const { REST, Routes } = require("discord.js");
+require("dotenv").config();
+
 
 const commands = [
-  new SlashCommandBuilder()
-    .setName("raid")
-    .setDescription("Stuur een bericht meerdere keren")
-    .addStringOption(option =>
-      option
-        .setName("bericht")
-        .setDescription("Het bericht dat gestuurd wordt")
-        .setRequired(true)
-    ),
+{
+name: "raid",
+description: "Stuur 10x een bericht in dit kanaal",
+options: [
+{
+name: "bericht",
+description: "Wat moet er 10x gestuurd worden?",
+type: 3,
+required: true
+}
+]
+},
+{
+name: "blame",
+description: "Geef iemand een blame",
+options: [
+{
+name: "user",
+description: "Wie wil je blamen?",
+type: 6,
+required: true
+}
+]
+}
+];
 
-  new SlashCommandBuilder()
-    .setName("blame")
-    .setDescription("Stuur een bericht naar een user (privé)")
-    .addUserOption(option =>
-      option
-        .setName("persoon")
-        .setDescription("Welke gebruiker wil je blame'en?")
-        .setRequired(true)
-    )
-].map(cmd => cmd.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
+
 (async () => {
-  try {
-    console.log("📡 Commands registreren...");
-    await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-      { body: commands }
-    );
-    console.log("✅ Commands succesvol geregistreerd!");
-  } catch (err) {
-    console.error(err);
-  }
+try {
+console.log("Global slash commands registreren...");
+await rest.put(
+Routes.applicationCommands(process.env.CLIENT_ID),
+{ body: commands }
+);
+console.log("KLAAR! Commands worden nu overal uitgerold.");
+} catch (e) {
+console.error(e);
+}
 })();
